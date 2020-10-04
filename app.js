6cfb2ac3daf587c4cf44bcde50b8d50b63e3591b
Pavan S <48400643+PavanS27@users.noreply.github.com>
2020-10-04 19:45:21 +0530

    init creation

---
+++ app.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const width = 8;
+  const squares = [];
+
+  const canduColors = ["red", "yellow", "orange", "purple", "green", "blue"];
+
+  function createBoard() {
+    for (let i = 0; i < width * width; i++) {
+      const square = document.createElement("div");
+      square.setAttribute("draggable", true);
+      square.setAttribute("id", i);
+      let randomColor = Math.floor(Math.random() * canduColors.length);
+      square.style.backgroundColor = canduColors[randomColor];
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+
+  let colorBeingDragged;
+
+  squares.forEach((square) => square.addEventListener("dragstart", dragStart));
+  squares.forEach((square) => square.addEventListener("dragend", dragEnd));
+  squares.forEach((square) => square.addEventListener("dragover", dragOver));
+  squares.forEach((square) => square.addEventListener("dragenter", dragEnter));
+  squares.forEach((square) => square.addEventListener("dragleave", dragLeave));
+  squares.forEach((square) => square.addEventListener("drop", dragDrop));
+
+  function dragStart() {
+    colorBeingDragged = this.style.backgroundColor;
+    console.log(colorBeingDragged);
+    console.log(this.id, "dragstart");
+  }
+  function dragEnd() {
+    console.log(this.id, "dragend");
+  }
+  function dragOver() {
+    console.log(this.id, "dragover");
+  }
+  function dragEnter() {
+    console.log(this.id, "dragenter");
+  }
+  function dragLeave() {
+    console.log(this.id, "dragleave");
+  }
+  function dragDrop() {
+    console.log(this.id, "drop");
+  }
+});
